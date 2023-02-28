7e21d86e68d463b14332940763b0c7aed39d8190
Vitor RS <vitor2000rs@gmail.com>
2023-02-28 17:41:47 -0300

    Commit inicial

---
+++ data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        title: "Santiago",
+        location: "Chile",
+        googleMapsUrl: "https://goo.gl/maps/VagKvCrdUBbSyhsh6",
+        startDate: "18 Nov, 2016",
+        endDate: "24 Nov, 2016",
+        description: "Santiago, capital e maior cidade do Chile, fica em um vale circundado pelos Andes cobertos pela neve e a cadeia de montanhas chilena.",
+        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipMq_D3oGIiqwLO7NIp46cdVd6gqrmDXhUbkCPy9=w408-h270-k-no",
+        id: "0"
+    },
+    {
+        title: "Cataratas do Iguaçú",
+        location: "Paraná / Missiones",
+        googleMapsUrl: "https://goo.gl/maps/eDAxwvyCRsZNqF5T7",
+        startDate: "24 Set, 2022",
+        endDate: "27 Set, 2022",
+        description: "Cataratas do Iguaçu é um conjunto de cerca de 275 quedas de água no rio Iguaçu , localizada entre o Parque Nacional do Iguaçu  e o Parque Nacional Iguazú, na fronteira entre os dois países.",
+        imageUrl: "https://cdn.pixabay.com/photo/2013/11/29/21/01/foz-do-iguacu-221272_960_720.jpg",
+        id: "1"
+    },
+    {
+        title: "Barra do Sahy",
+        location: "São Paulo - Brazil",
+        googleMapsUrl: "https://goo.gl/maps/vy1G9KrQmfsGy7q37",
+        startDate: "11 Dec, 2022",
+        endDate: "12 Dec, 2022",
+        description: "A Barra do Sahy é uma pequena praia, considerada uma das mais belas de São Sebastião, é um lugar incrivelmente bonito, banhado por águas calmas e cristalinas.",
+        imageUrl: "https://www.ciribaipraiahotel.com.br/wp-content/uploads/cache/2021/02/Praia-da-Baleia-e-Barra-do-Sahy-1/2100322888.jpg",
+        id: "2"
+    }
+]
