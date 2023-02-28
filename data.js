9c653ba6c7f48f4dc459a0c1d0cce1d91120242a
Vitor RS <vitor2000rs@gmail.com>
2023-02-28 17:53:04 -0300

    .

--- data.js
+++ data.js
@@ -37,7 +37,7 @@ export default [
         endDate: "24 Nov, 2016",
         description: "Santiago, capital e maior cidade do Chile, fica em um vale circundado pelos Andes cobertos pela neve e a cadeia de montanhas chilena.",
         imageUrl: "https://lh5.googleusercontent.com/p/AF1QipMq_D3oGIiqwLO7NIp46cdVd6gqrmDXhUbkCPy9=w408-h270-k-no",
-        id: "0"
+        id: "3"
     },
     {
         title: "Cataratas do Iguaçú",
@@ -47,7 +47,7 @@ export default [
         endDate: "27 Set, 2022",
         description: "Cataratas do Iguaçu é um conjunto de cerca de 275 quedas de água no rio Iguaçu , localizada entre o Parque Nacional do Iguaçu  e o Parque Nacional Iguazú, na fronteira entre os dois países.",
         imageUrl: "https://cdn.pixabay.com/photo/2013/11/29/21/01/foz-do-iguacu-221272_960_720.jpg",
-        id: "1"
+        id: "4"
     },
     {
         title: "Barra do Sahy",
@@ -57,6 +57,6 @@ export default [
         endDate: "12 Dec, 2022",
         description: "A Barra do Sahy é uma pequena praia, considerada uma das mais belas de São Sebastião, é um lugar incrivelmente bonito, banhado por águas calmas e cristalinas.",
         imageUrl: "https://www.ciribaipraiahotel.com.br/wp-content/uploads/cache/2021/02/Praia-da-Baleia-e-Barra-do-Sahy-1/2100322888.jpg",
-        id: "2"
+        id: "5"
     }
 ]
