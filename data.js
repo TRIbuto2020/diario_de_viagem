1f9e097a5671ac34a9674b724d50fecdad130ba8
Vitor RS <vitor2000rs@gmail.com>
2023-02-28 18:07:01 -0300

    data update

--- data.js
+++ data.js
@@ -21,7 +21,7 @@ export default [
     },
     {
         title: "Barra do Sahy",
-        location: "São Paulo - Brazil",
+        location: "São Paulo - Brasil",
         googleMapsUrl: "https://goo.gl/maps/vy1G9KrQmfsGy7q37",
         startDate: "11 Dec, 2022",
         endDate: "12 Dec, 2022",
@@ -30,33 +30,33 @@ export default [
         id: "2"
     },
     {
-        title: "Santiago",
-        location: "Chile",
-        googleMapsUrl: "https://goo.gl/maps/VagKvCrdUBbSyhsh6",
-        startDate: "18 Nov, 2016",
-        endDate: "24 Nov, 2016",
-        description: "Santiago, capital e maior cidade do Chile, fica em um vale circundado pelos Andes cobertos pela neve e a cadeia de montanhas chilena.",
-        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipMq_D3oGIiqwLO7NIp46cdVd6gqrmDXhUbkCPy9=w408-h270-k-no",
+        title: "Blumenau",
+        location: "Santa Catarina - Brasil",
+        googleMapsUrl: "https://goo.gl/maps/kJXsZyq7oqa6Pt4ZA",
+        startDate: "18 Out, 2015",
+        endDate: "19 Out, 2015",
+        description: "Blumenau é um município do estado de Santa Catarina, Região Sul do Brasil. Localiza-se na microrregião homônima e na Mesorregião do Vale do Itajaí.",
+        imageUrl: "https://www.viagensecaminhos.com/wp-content/uploads/2014/08/blumenau-oktoberfest.jpg",
         id: "3"
     },
     {
-        title: "Cataratas do Iguaçú",
-        location: "Paraná / Missiones",
-        googleMapsUrl: "https://goo.gl/maps/eDAxwvyCRsZNqF5T7",
-        startDate: "24 Set, 2022",
-        endDate: "27 Set, 2022",
-        description: "Cataratas do Iguaçu é um conjunto de cerca de 275 quedas de água no rio Iguaçu , localizada entre o Parque Nacional do Iguaçu  e o Parque Nacional Iguazú, na fronteira entre os dois países.",
-        imageUrl: "https://cdn.pixabay.com/photo/2013/11/29/21/01/foz-do-iguacu-221272_960_720.jpg",
+        title: "Penápolis",
+        location: "São Paulo - Brasil",
+        googleMapsUrl: "https://goo.gl/maps/RhZ3Dtx2yAUzJRoY7",
+        startDate: "19 Fev, 2023",
+        endDate: "20 Fev, 2023",
+        description: "Penápolis é um município brasileiro do estado de São Paulo. Sua população estimada em 2018 era de 63 047 habitantes.",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Museu_do_Sol_%28Pen%C3%A1polis%29.jpg",
         id: "4"
     },
     {
-        title: "Barra do Sahy",
+        title: "Ubatuba",
         location: "São Paulo - Brazil",
-        googleMapsUrl: "https://goo.gl/maps/vy1G9KrQmfsGy7q37",
-        startDate: "11 Dec, 2022",
-        endDate: "12 Dec, 2022",
-        description: "A Barra do Sahy é uma pequena praia, considerada uma das mais belas de São Sebastião, é um lugar incrivelmente bonito, banhado por águas calmas e cristalinas.",
-        imageUrl: "https://www.ciribaipraiahotel.com.br/wp-content/uploads/cache/2021/02/Praia-da-Baleia-e-Barra-do-Sahy-1/2100322888.jpg",
+        googleMapsUrl: "https://goo.gl/maps/nFS4JZiP5Fex6LPd7",
+        startDate: "11 Set, 2021",
+        endDate: "12 Set, 2021",
+        description: "Em Ubatuba, o projeto TAMAR protege as tartarugas e tem um centro de visitantes com tanques. Existem mais criaturas marinhas em exibição no Aquário de Ubatuba.",
+        imageUrl: "https://www.tamar.org.br/fotos/ubatuba-dia-crianca.jpg",
         id: "5"
     }
 ]
